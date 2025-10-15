0f8ad7344692609d013e51fb7d0bdfd9b7af560c
vedantShah30 <pravincj27@gmail.com>
2025-10-16 02:10:14 +0530

    folder structure

---
+++ backend/src/db/config.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+export const connectDatabase = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1);
+  }
+};
+
+export const disconnectDatabase = async () => {
+  try {
+    await prisma.$disconnect();
+    console.log("Database disconnected successfully");
+  } catch (error) {
+    console.error("Error disconnecting from database:", error);
+  }
+};
+export default prisma;
---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv"
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res)=>{
+    res.send("Hi there");
+});
+app.listen(process.env.PORT, ()=>{
+    console.log(`server listening on port ${process.env.PORT}`)
+});
+
