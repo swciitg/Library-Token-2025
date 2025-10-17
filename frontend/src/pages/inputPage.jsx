f43a8cbebadb0317a062a11e51cd1d3dc9e322ec
sowwmyaa <sowmyasri1019@gmail.com>
2025-10-17 21:51:14 +0530

    tailwind added

--- frontend/src/pages/inputPage.jsx
+++ frontend/src/pages/inputPage.jsx
@@ -5,7 +5,8 @@ import Shelfs from "../components/shelf";
 export default function inputPage(){
     return (
         <div>
-            <h1>Slot Allocation</h1>
+            <h1 className="hover:text-gray-300 transition-all duration-150">Slot Allocation</h1>
+
             <RollEntry />
             {/* <Shelfs showSlot={showSlot} status={status}/> */}
         </div>
