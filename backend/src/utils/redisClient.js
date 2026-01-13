ee433cc80a3dd066eb3437ed56f92e32f461a7c7
vedantShah30 <pravincj27@gmail.com>
2026-01-13 17:14:37 +0530

    fix: added username and password to bypass NOAUTH Authentication required error

--- backend/src/utils/redisClient.js
+++ backend/src/utils/redisClient.js
@@ -2,7 +2,10 @@ import Redis from "ioredis";
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
+  port: process.env.REDIS_PORT,
+  // remove this while deploying
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
 });
 
 redisClient.on("connect", () => console.log("-- Connected to Redis"));
