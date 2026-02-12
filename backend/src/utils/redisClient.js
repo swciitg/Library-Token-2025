c7f28c2e7915f5f5771eab254eb144a2c1ae3f26
SWCIITGWeb <swciitghy@gmail.com>
2026-02-12 10:14:38 +0000

    fix

--- backend/src/utils/redisClient.js
+++ backend/src/utils/redisClient.js
@@ -4,8 +4,6 @@ const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   // remove this while deploying
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
 });
 
 redisClient.on("connect", () => console.log("-- Connected to Redis"));
