f6e7a4e8a7e2cf88850cd8fae39919ea9ddc1d32
vedantShah30 <pravincj27@gmail.com>
2026-03-18 00:46:01 +0530

    fix: revert

--- backend/src/controllers/entryController.js
+++ backend/src/controllers/entryController.js
@@ -111,23 +111,23 @@ export const checkStudentStatus = async (req, res) => {
     const diffHours = diffMs / (1000 * 60 * 60);
 
     // 0 - 24 hrs
-    // if (diffHours <= 24) {
-    //   return res.status(200).json({
-    //     message: null,
-    //     isBanned: false,
-    //     slotId: entry.slotId,
-    //   });
-    // }
+    if (diffHours <= 24) {
+      return res.status(200).json({
+        message: null,
+        isBanned: false,
+        slotId: entry.slotId,
+      });
+    }
 
-    // // 24 - 48 hrs
-    // if (diffHours > 24 && diffHours <= 48) {
-    //   const remaining = Math.ceil(48 - diffHours);
-    //   return res.status(200).json({
-    //     message: `Collect your bag in ${remaining} hrs`,
-    //     isBanned: false,
-    //     slotId: entry.slotId,
-    //   });
-    // }
+    // 24 - 48 hrs
+    if (diffHours > 24 && diffHours <= 48) {
+      const remaining = Math.ceil(48 - diffHours);
+      return res.status(200).json({
+        message: `Collect your bag in ${remaining} hrs`,
+        isBanned: false,
+        slotId: entry.slotId,
+      });
+    }
 
     // > 48 hrs
     if (diffHours >=0){
